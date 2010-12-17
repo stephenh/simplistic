241c2feaad08102c92f6ced25e99a8b3290d5e41
Alex Boisvert <alex.boisvert@gmail.com>
2010-12-17 12:17:24 -0800

    Cosmetic reformatting

--- src/main/scala/simplistic/Quoting.scala
+++ src/main/scala/simplistic/Quoting.scala
@@ -1,39 +1,29 @@
-package simplistic {
+package simplistic
 
-    object Quoting {
+object Quoting {
 
-        /**
-         * Generic quoting function
-         */
-        def quote (string:String, quote:Char, rules: Char => Any) =
-            quote + ((string map {rules}) mkString) + quote
+  /** Generic quoting function */
+  def quote(string: String, quote: Char, rules:  Char => Any) = {
+    quote + (string map rules mkString) + quote
+  }
 
-        /**
-         * Quote a value.
-         */
-        def quoteValue (value:String) = quote(value, '\'', valueRules)
+  /** Quote a value. */
+  def quoteValue(value: String) = quote(value, '\'', valueRules)
 
-        /**
-         * Quote an attribute name.
-         */
-        def quoteName (name:String) = quote(name, '`', attributeRules)
+  /** Quote an attribute name. */
+  def quoteName(name: String) = quote(name, '`', attributeRules)
 
-        /**
-         * Rules for quoting attribute names.
-         */
-        def attributeRules (a:Char) = a match {
-            case '`' => "``"
-            case a:Char => a
-        }
+  /** Rules for quoting attribute names. */
+  def attributeRules(a: Char) = a match {
+    case '`' => "``"
+    case a: Char => a
+  }
 
-        /**
-         * Rules for quoting values.
-         */
-        def valueRules (a:Char) = a match {
-            case '"' => "\"\""
-            case '`' => "``"
-            case '\'' => "''"
-            case a:Char => a
-        }
-    }
+  /** Rules for quoting values. */
+  def valueRules(a: Char) = a match {
+    case '"' => "\"\""
+    case '`' => "``"
+    case '\'' => "''"
+    case a: Char => a
+  }
 }
